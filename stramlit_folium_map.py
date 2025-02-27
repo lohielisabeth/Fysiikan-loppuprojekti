8ef303daafec0cc51a72f2dcadba0d17003ee5fb
Elisabeth Lohi <ellipetti01@hotmail.com>
2025-02-27 11:45:17 +0200

    vaiheessa

--- stramlit_folium_map.py
+++ stramlit_folium_map.py
@@ -3,3 +3,43 @@ import pandas as pd
 import folium 
 from streamlit_folium import st_folium
 
+path1 = "Location.csv"
+df = pd.read_csv(path1)
+
+path2 = "LinearAccelerometer.csv"
+df2 = pd.read_csv(path2)
+
+st.title('Tulokset')
+
+from scipy.signal import butter, filtfilt
+def butter_lowpass_filter(data, cutoff, fs, nyq, order): 
+    normal_cutoff = cutoff / nyq
+
+    b, a = butter(order, normal_cutoff, btype='low', analog=False)
+    y = filtfilt(b, a, data)
+    return y
+
+def butter_highpass_filter(data, cutoff, fs, nyq, order): 
+    normal_cutoff = cutoff / nyq
+
+    b, a = butter(order, normal_cutoff, btype='high', analog=False)
+    y = filtfilt(b, a, data)
+    return y
+
+T = df2['Time (s)'][len(df2['Time (s)'])-1] - df2['Time (s)'][0]
+n = len(df2['Time (s)'])
+fs = n/T
+nyq = fs/2
+order = 3
+cutoff = 1/(0.2)
+
+df2['filter_a_z'] = butter_lowpass_filter( df2['Z (m/s^2)'], cutoff, fs, nyq, order)
+
+from scipy.signal import find_peaks
+
+peaks, _ = find_peaks(df2['filter_a_z'], distance=fs*0.2) 
+
+valleys, _ = find_peaks(-df2['filter_a_z'], distance=fs*0.2)
+total_steps = min(len(peaks), len(valleys))
+
+st.write("Askelmäärä laskettuna suodatuksen avulla:",total_steps, "askelta")
